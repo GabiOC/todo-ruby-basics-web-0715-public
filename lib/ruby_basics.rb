fe194544846ff33df5734dc91a0325232cf1bf62
GabiOC <oconnor.gabriele@gmail.com>
2015-07-29 15:36:40 -0400

    Add code to methods to pass failing tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,26 @@
 def division(num1, num2)
- 
+ 	num1 / num2
 end
 
 def assign_variable(value)
-  
+  variable = value
 end
 
-def argue
-  
+def argue(x)
+  x
 end
 
-def greeting
-
+def greeting(x, y)
 end
 
 def return_a_value
-
+	return "Nice"
 end
 
 def last_evaluated_value
-
+	return "expert"
 end
 
-def pizza_party
-
+def pizza_party(x = "cheese", y = "pepperoni")
+	return x
 end
